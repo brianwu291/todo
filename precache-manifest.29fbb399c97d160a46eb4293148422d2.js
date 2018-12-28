929fff4ae3f892d94f8754dcd251dfad2abf026f
BrianWu <brianwu291@gmail.com>
2018-12-29 03:41:20 +0800

    Updates

---
+++ precache-manifest.29fbb399c97d160a46eb4293148422d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "5407584b8aceaf251a54",
+    "url": "/todo/static/js/main.5407584b.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/todo/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "337681be845fbf16aaf7ae79e553b9cf",
+    "url": "/todo/index.html"
+  }
+];
